cbae89a8ab6d83403f94c725e1bd55db351ae46c
bjeptum <brendajeptum@gmail.com>
2024-10-17 22:32:49 +0000

    Create table with unique attributes

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- Create table users with unique attributes
-CREATE DATABASE if not exists users unique_id(
+CREATE DATABASE if not exists users(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
