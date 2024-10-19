7548e4c890d7dd325ff779b02a9096c268ef9a76
bjeptum <brendajeptum@gmail.com>
2024-10-19 21:55:49 +0000

    Rank column by number of fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,11 +1,7 @@
 -- Ranks country origins of bands --
 -- Ordered by number of fans(non-unique --
-SELECT origin,
-       SUM(nb_fans) as total_fans
-FROM
-	metal_bands
-GROUP BY
-	origin
-ORDER BY
-	total_fans DESC;
+SELECT origin, SUM(fans) as nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
 
